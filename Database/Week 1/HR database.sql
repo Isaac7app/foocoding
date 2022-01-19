1d5a0b79ca6dae677f762acdd8d03b38b4513d42
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-19 11:09:55 +0100

    Hr database created

---
+++ Database/Week 1/HR database.sql
@@ -0,0 +1,26 @@
+/*
+-- Query: SELECT * FROM sql_hr.employees
+LIMIT 0, 1000
+
+-- Date: 2022-01-19 11:05
+*/
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (33391,'D\'arcy','Nortunen','Account Executive',62871,37270,1);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (37270,'Yovonnda','Magrannell','Executive Secretary',63996,NULL,10);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (37851,'Sayer','Matterson','Statistician III',98926,37270,1);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (40448,'Mindy','Crissil','Staff Scientist',94860,37270,1);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (56274,'Keriann','Alloisi','VP Marketing',110150,37270,1);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (63196,'Alaster','Scutchin','Assistant Professor',32179,37270,2);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (67009,'North','de Clerc','VP Product Management',114257,37270,2);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (67370,'Elladine','Rising','Social Worker',96767,37270,2);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (68249,'Nisse','Voysey','Financial Advisor',52832,37270,2);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (72540,'Guthrey','Iacopetti','Office Assistant I',117690,37270,3);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (72913,'Kass','Hefferan','Computer Systems Analyst IV',96401,37270,3);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (75900,'Virge','Goodrum','Information Systems Manager',54578,37270,3);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (76196,'Mirilla','Janowski','Cost Accountant',119241,37270,3);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (80529,'Lynde','Aronson','Junior Executive',77182,37270,4);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (80679,'Mildrid','Sokale','Geologist II',67987,37270,4);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (84791,'Hazel','Tarbert','General Manager',93760,37270,4);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (95213,'Cole','Kesterton','Pharmacist',86119,37270,4);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (96513,'Theresa','Binney','Food Chemist',47354,37270,5);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (98374,'Estrellita','Daleman','Staff Accountant IV',70187,37270,5);
+INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (115357,'Ivy','Fearey','Structural Engineer',92710,37270,5);
