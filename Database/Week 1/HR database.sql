89607d8dc7df1fda66de717baf1ecb36602cbef0
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-21 11:32:43 +0100

    added CREATE DATABASE statement

--- Database/Week 1/HR database.sql
+++ Database/Week 1/HR database.sql
@@ -4,8 +4,8 @@ LIMIT 0, 1000
 
 -- Date: 2022-01-19 11:05
 */
-CREATE DATABASE `sql_hr`
-CREATE TABLE 'employees'
+CREATE DATABASE sql_hr;
+
 
 INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (33391,'D\'arcy','Nortunen','Account Executive',62871,37270,1);
 INSERT INTO `` (`employee_id`,`first_name`,`last_name`,`job_title`,`salary`,`reports_to`,`office_id`) VALUES (37270,'Yovonnda','Magrannell','Executive Secretary',63996,NULL,10);
