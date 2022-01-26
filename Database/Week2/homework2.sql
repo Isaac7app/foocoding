f8c59b62f01b2262b56a2f7b46733be802653fa7
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-26 17:59:19 +0100

    working on part 2

--- Database/Week2/homework2.sql
+++ Database/Week2/homework2.sql
@@ -1,10 +1,17 @@
 // Question 2
+CREATE DATABASE IF NOT EXITS todo;
 
-CREATE TABLE todo_list (id INTEGER PRIMARY KEY, item TEXT, minutes INTEGER);
+CREATE TABLE IF NOT EXITS todo_list1 (id INTEGER PRIMARY KEY, activity TEXT, minutes INTEGER);
 INSERT INTO todo_list VALUES (1, "Wash the dishes", 15);
 INSERT INTO todo_list VALUES (2, "vacuuming", 20);
-INSERT INTO todo_list VALUES (3, "Learn some stuff on KA", 30);
+INSERT INTO todo_list VALUES (3, "Learn some stuff on mysql", 60);
 INSERT INTO todo_list VALUES (4, "Eat", 25);
 
+
+CREATE TABLE IF NOT EXITS todo_list2 (id INTEGER PRIMARY KEY , item TEXT, amount INTEGER );
+INSERT INTO todo_list VALUES (1, "bread", 15);
+INSERT INTO todo_list VALUES (2, "milk", 20);
+INSERT INTO todo_list VALUES (3, "fruits", 30);
+INSERT INTO todo_list VALUES (4, "meat", 25);
 
 
