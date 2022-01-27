813f261985969be469886779abc35b29ec8ec78a
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-27 08:43:37 +0100

    corrections made

--- Database/Week2/homework1.sql
+++ Database/Week2/homework1.sql
@@ -1,18 +1,18 @@
 -- 1. What is the capital of country X ? (Accept X from user)
 
 prepare city from 'select ci.name from city ci inner join country co on 
-ci.countrycode = co.code where lower(co.name) = ? limit 1';
+ci.countrycode = co.code where lower(co.name) = ? ';
 
 set @a = 'sweden';
 execute city using @a;
 
 -- 2. List all the languages spoken in the region Y (Accept Y from user)
 
-prepare lang from 'select countrylanguage.Language from countrylanguage inner join country on 
+prepare language from 'select countrylanguage.Language from countrylanguage inner join country on 
 countrylanguage.CountryCode = country.code where Region = ?';
 
 set @a = 'Western Africa';
-execute lang using @a;
+execute language using @a;
 
 -- 3. Find the number of cities in which language Z is spoken (Accept Z from user)
 
